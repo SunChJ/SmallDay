6b1daea10f09a65e54b5d18fadd4f59b5077b933
SunCJ <35075200@qq.com>
2016-04-06 11:07:38 +0800

    Reday To Start

---
+++ SmallDay/a.hpp
@@ -0,0 +1,14 @@
+//
+//  a.hpp
+//  SmallDay
+//
+//  Created by 孙超杰 on 16/4/6.
+//  Copyright © 2016年 孙超杰. All rights reserved.
+//
+
+#ifndef a_hpp
+#define a_hpp
+
+#include <stdio.h>
+
+#endif /* a_hpp */
